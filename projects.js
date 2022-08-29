d69a51339a6e362f41e32067d73a801b83984305
DevWeb13 <giuliano.loic@sfr.fr>
2022-08-29 17:51:41 +0200

    feat: Add 1 project

--- projects.js
+++ projects.js
@@ -1,28 +1,12 @@
 const projects = [
   {
-    name: 'Kasa',
-    description: 'Développez une application Web avec React et React Router',
-    image: 'https://zupimages.net/up/22/34/5aib.png',
-    link: 'https://giuliano-loic-11-07022022.vercel.app/',
-    technologies: ['React', 'Sass', 'js'],
-    gitHub:'https://github.com/DevWeb13/GiulianoLoic_11_07022022'
-  },
-  {
-    name: 'FishEye',
-    description: 'Créez un site accessible pour une plateforme de photographes.',
-    image: 'https://zupimages.net/up/22/34/ux6c.png',
-    link: 'https://devweb13.github.io/GiulianoLoic_6_05112021/www/',
-    technologies: ['html', 'css', 'js', 'gulp', 'sass', 'webpack', 'accessibility'],
-    gitHub: 'https://github.com/DevWeb13/GiulianoLoic_6_05112021'
-  },
-  {
-    name: 'Reservia',
-    description: 'Transformez une maquette en site web.',
-    image: 'https://zupimages.net/up/22/35/suwl.png',
-    link: 'https://devweb13.github.io/GiulianoLoic_2_11082021/',
-    technologies: ['html', 'css'],
-    gitHub: 'https://github.com/DevWeb13/GiulianoLoic_2_11082021'
-  },
+    name: 'Ohmyfood!',
+    description: 'Dynamisez une page web avec des animations CSS',
+    image: 'https://zupimages.net/up/22/35/oba3.png',
+    link: 'https://devweb13.github.io/GiulianoLoic_3_26082021/',
+    technologies: ['html', 'css', 'sass'],
+    gitHub: 'https://github.com/DevWeb13/GiulianoLoic_3_26082021'
+  }
   
 ]
 

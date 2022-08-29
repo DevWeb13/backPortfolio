9c40fa4ee2ebd47f0e4576f2ae07dc204f21398b
DevWeb13 <giuliano.loic@sfr.fr>
2022-08-29 10:43:28 +0200

    feat: (projects.js): add reservia

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     name: 'Kasa',
     description: 'Développez une application Web avec React et React Router',
-    image: 'kasa.png',
+    image: 'https://zupimages.net/up/22/34/5aib.png',
     link: 'https://giuliano-loic-11-07022022.vercel.app/',
     technologies: ['React', 'Sass', 'js'],
     gitHub:'https://github.com/DevWeb13/GiulianoLoic_11_07022022'
@@ -10,10 +10,18 @@ const projects = [
   {
     name: 'FishEye',
     description: 'Créez un site accessible pour une plateforme de photographes.',
-    image: 'assets/fisheye.png',
+    image: 'https://zupimages.net/up/22/34/ux6c.png',
     link: 'https://devweb13.github.io/GiulianoLoic_6_05112021/www/',
     technologies: ['html', 'css', 'js', 'gulp', 'sass', 'webpack', 'accessibility'],
     gitHub: 'https://github.com/DevWeb13/GiulianoLoic_6_05112021'
+  },
+  {
+    name: 'Reservia',
+    description: 'Transformez une maquette en site web.',
+    image: 'https://zupimages.net/up/22/35/suwl.png',
+    link: 'https://devweb13.github.io/GiulianoLoic_2_11082021/',
+    technologies: ['html', 'css'],
+    gitHub: 'https://github.com/DevWeb13/GiulianoLoic_2_11082021'
   },
   
 ]

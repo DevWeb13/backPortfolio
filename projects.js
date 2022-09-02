00e98bdb92a4fcc2130ce3c884f6cad7e1ccdbf8
DevWeb13 <giuliano.loic@sfr.fr>
2022-09-02 21:29:55 +0200

    feat: (projects.js): add OCR projects

--- projects.js
+++ projects.js
@@ -1,13 +1,75 @@
 const projects = [
   {
+    name: 'Les petits plats',
+    description: 'Développez un algorithme de recherche en JavaScript',
+    image: 'https://zupimages.net/up/22/35/cafy.png',
+    link: 'https://giulianoloic-7-30062022-b5y53iykx-devweb13.vercel.app/',
+    technologies: ['react', 'js', 'sass', 'node', 'express', 'vercel', 'jsdoc'],
+    gitHub: 'https://github.com/DevWeb13/giulianoloic_7_30062022',
+    date: "2022-07",
+    categorie: "OpenClassrooms",
+  },
+  {
+    name: 'Wealth Health',
+    description: 'Faites passer une librairie jQuery vers React',
+    image: 'https://zupimages.net/up/22/35/gqu2.png',
+    link: 'https://giulianoloic-14-28052022.vercel.app/',
+    technologies: ['react', 'js', 'sass', 'node', 'express', 'vercel', 'jsdoc'],
+    gitHub: 'https://github.com/DevWeb13/giulianoloic_14_28052022',
+    date: "2022-06",
+    categorie: "OpenClassrooms",
+  },
+  {
+    name: 'SportSee',
+    description: 'Développez un tableau de bord d\'analytics avec React',
+    image: 'https://zupimages.net/up/22/35/3jsa.png',
+    link: 'https://giuliano-loic-12-26022022-hnc6ozb63-devweb13.vercel.app/',
+    technologies: ['react', 'js', 'sass', 'node', 'recharts', 'vercel', 'jsdoc'],
+    gitHub: 'https://github.com/DevWeb13/GiulianoLoic_12_26022022',
+    date: "2022-03",
+    categorie: "OpenClassrooms",
+  },
+  {
+    name: "Kasa",
+    description: "Développez une application Web avec React et React Router",
+    image: "https://zupimages.net/up/22/34/5aib.png",
+    link: "https://giuliano-loic-11-07022022.vercel.app/",
+    technologies: ["react", "sass", "js", "vercel", 'jsdoc'],
+    gitHub: "https://github.com/DevWeb13/GiulianoLoic_11_07022022",
+    date: "2022-03",
+    categorie: "OpenClassrooms",
+  },
+  {
+    name: "FishEye",
+    description: "Créez un site accessible pour une plateforme de photographes",
+    image: "https://zupimages.net/up/22/34/ux6c.png",
+    link: "https://devweb13.github.io/GiulianoLoic_6_05112021/www/",
+    technologies: ["html", "css", "js", "gulp", "sass", "webpack", "accessibility"],
+    gitHub: "https://github.com/DevWeb13/GiulianoLoic_6_05112021",
+    date: "2022-01",
+    categorie: "OpenClassrooms",
+  },
+  {
     name: 'Ohmyfood!',
     description: 'Dynamisez une page web avec des animations CSS',
     image: 'https://zupimages.net/up/22/35/oba3.png',
     link: 'https://devweb13.github.io/GiulianoLoic_3_26082021/',
     technologies: ['html', 'css', 'sass'],
-    gitHub: 'https://github.com/DevWeb13/GiulianoLoic_3_26082021'
-  }
-  
+    gitHub: 'https://github.com/DevWeb13/GiulianoLoic_3_26082021',
+    date: "2021-09",
+    categorie: "OpenClassrooms",
+  },
+  {
+    name: "Reservia",
+    description: "Transformez une maquette en site web",
+    image: "https://zupimages.net/up/22/35/1xvl.png",
+    link: "https://devweb13.github.io/GiulianoLoic_2_11082021/",
+    technologies: ["html", "css"],
+    gitHub: "https://github.com/DevWeb13/GiulianoLoic_2_11082021",
+    date: "2021-08",
+    categorie: "OpenClassrooms",
+
+  },
 ]
 
 module.exports = projects;

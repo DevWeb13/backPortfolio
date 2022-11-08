9be41d6bdd1cd9b26273d9911034d63e6aa571f9
DevWeb13 <giuliano.loic@sfr.fr>
2022-11-08 12:57:49 +0100

    fix: (Comments.js): date: { type: String, required: true },

--- models/Comments.js
+++ models/Comments.js
@@ -6,7 +6,7 @@ const commentSchema = new mongoose.Schema({
   nom: { type: String, required: true },
   message: { type: String, required: true },
   email: { type: String, required: true },
-  date: { type: Date, default: new Date() },
+  date: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
